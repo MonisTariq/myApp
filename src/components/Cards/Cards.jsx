5236ecffbefe0a8f113490d67117f0c4cb7e68cf
Monis Tariq <monistariq3@gmail.com>
2020-07-08 00:55:32 +0530

    api addition left

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,12 +1,47 @@
 import React from 'react';
 import {Card, CardContent, Typography, Grid} from '@material-ui/core';
-const Cards = (props) =>
+
+import styles from './Cards.module.css';
+
+const Cards = ({confirmed,recovered,deaths,lastUpdate}) =>
 {
-	    console.log(props);
+	    if(!confirmed)
+			{
+				return 'Loading......';
+			}
 
 return(
 
-<h1> Cards</h1>
+<div className={styles .container}>
+	<Grid container spacng={3} justify="center">
+		<Grid item component={Card}>
+			<CardContent>
+				<Typography color="textSecondary" gutterBottom>Infected</Typography>
+				<Typography varient ="h5"> confirmed.value </Typography>
+				<Typography color="textSecondary"> REAL DATE </Typography>
+				<Typography varient="body2"> Number of active cases of COVID-19</Typography>
+			</CardContent>
+		</Grid>
+		<Grid item component={Card}>
+			<CardContent>
+				<Typography color="textSecondary" gutterBottom>Recovered</Typography>
+				<Typography varient ="h5"> REAL DATA </Typography>
+				<Typography color="textSecondary"> REAL DATE </Typography>
+				<Typography varient="body2"> Number of recoveries from COVID-19</Typography>
+			</CardContent>
+		</Grid>
+		<Grid item component={Card}>
+			<CardContent>
+				<Typography color="textSecondary" gutterBottom>Deaths</Typography>
+				<Typography varient ="h5"> REAL DATA </Typography>
+				<Typography color="textSecondary"> REAL DATE </Typography>
+				<Typography varient="body2"> Number of deaths due to COVID-19</Typography>
+			</CardContent>
+		</Grid>
+	</Grid>
+</div>	
+
+
 )
 
 }
